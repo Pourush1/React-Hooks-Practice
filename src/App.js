1e8eedc0cca596bcc856563609231889e4c3fea7
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-06-17 20:30:07 -0700

    basic practice of react-hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [userName, setUsername] = useState("");
+  const [passWord, setUserPassword] = useState("");
+  const [showPassword, setShowPassWord] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Enter the userName"
+        value={userName}
+        onChange={e => {
+          setUsername(e.target.value);
+        }}
+      />
+      <input
+        type={showPassword ? "text" : "password"}
+        placeholder="Enter the password"
+        value={passWord}
+        onChange={e => {
+          setUserPassword(e.target.value);
+        }}
+      />
+
+      <h2>{userName}</h2>
+      {/* if the show password is true, dpsplay password else not */}
+      <h2>{showPassword ? passWord : "*".repeat(passWord.length)}</h2>
+
+      <button
+        onClick={e => {
+          setShowPassWord(!showPassword);
+        }}
+      >
+        {!showPassword ? "Show Password" : "Hide Password"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
